7435c7449dc60ea42bd2af89aa13b0a901ced436
nermienkh <nermien.khalifa@gmail.com>
2022-07-22 22:42:24 +0200

    - removed first column [Entrez Gene ID]

--- Part_1_Hypothesis.R
+++ Part_1_Hypothesis.R
@@ -9,18 +9,21 @@ GE_lusc.cancer <- read.table("./Project_Data/lusc-rsem-fpkm-tcga-t_paired.txt", 
 GE_lusc.healthy <- read.table("./Project_Data/lusc-rsem-fpkm-tcga_paired.txt", header = T, row.names=1)
 
 #1- clean Kirc dataframes
+##Remove the first column from data farmes
+GE_kirc.cancer =GE_kirc.cancer [,-1]
+GE_kirc.healthy=GE_kirc.healthy[,-1]
 
 ## Remove the Genes that have more than 50% of its samples as zeros either in healthy or Cancerous tissues.
 ## We have 68 patients ==> 50% of patients = 34 ==> If number of zeros in GE level of Healthy or Cancerous data > =35 ==> remove this Gene
 ## skip the Gene index if the GE levels for this Gene in Cancerous tissues are more than 50% zeros ==> 35 or more patient
-index_cancer = which(apply(GE_kirc.cancer == 0, 1, sum) >=35)
+index_cancer = which(apply(GE_kirc.cancer == 0, 1, sum) >=34)
 ## skip the Gene index if the GE levels for this Gene in Healthy tissues are more than 50% zeros ==> 35 or more patient
-index_healthy = which(apply(GE_kirc.healthy == 0, 1, sum) >=35)
+index_healthy = which(apply(GE_kirc.healthy == 0, 1, sum) >=34)
 
 ## Here are the unique indices for Genes that need to be skipped
 skip_index = unique(c(index_cancer, index_healthy))
 
-## Remove Genes from both Healthy and Cancerous tissues GE levels.
+## Remove Genes from both Healthy and Cancerous tissues GE levels. 
 GE_kirc.cancer.clean = GE_kirc.cancer[-skip_index,]
 GE_kirc.healthy.clean = GE_kirc.healthy[-skip_index,]
 
@@ -36,11 +39,12 @@ for(Gene in rownames(GE_kirc.cancer.clean))
   ## index of patients with GE level of Cancerous tissues = 0
   patient_index2 = which(GE_kirc.cancer.clean[Gene,]==0)
   
+  #combine patient_index1, patient_index2 to skip them
   skip_patient_index = unique(c(patient_index1, patient_index2))
   if (length(skip_patient_index)!=0)
   {
-  GE_kirc.cancer.clean2 = GE_kirc.cancer.clean2[-skip_patient_index]
-  GE_kirc.healthy.clean2 = GE_kirc.healthy.clean2[-skip_patient_index]
+    GE_kirc.cancer.clean2 = GE_kirc.cancer.clean2[-skip_patient_index]
+    GE_kirc.healthy.clean2 = GE_kirc.healthy.clean2[-skip_patient_index]
   }
   else
   {
@@ -55,12 +59,15 @@ for(Gene in rownames(GE_kirc.cancer.clean))
 }
 
 #2- clean Lusc dataframes 
+##Remove the first column from data farmes
+GE_lusc.cancer = GE_lusc.cancer[,-1]
+GE_lusc.healthy=GE_lusc.healthy[,-1]
 
 ## Remove the Genes that have more than 50% of its samples as zeros either in healthy or Cancerous tissues.
-## We have 50 patients ==> 50% of patients = 25 ==> If number of zeros in GE level of Healthy or Cancerous data > =35 ==> remove this Gene
-## skip the Gene index if the GE levels for this Gene in Cancerous tissues are more than 50% zeros ==> 35 or more patient
+## We have 50 patients ==> 50% of patients = 25 ==> If number of zeros in GE level of Healthy or Cancerous data > =25 ==> remove this Gene
+## skip the Gene index if the GE levels for this Gene in Cancerous tissues are more than 50% zeros ==> 25 or more patient
 index_cancer = which(apply(GE_lusc.cancer == 0, 1, sum) >=25)
-## skip the Gene index if the GE levels for this Gene in Healthy tissues are more than 50% zeros ==> 35 or more patient
+## skip the Gene index if the GE levels for this Gene in Healthy tissues are more than 50% zeros ==> 25 or more patient
 index_healthy = which(apply(GE_lusc.healthy == 0, 1, sum) >=25)
 
 ## Here are the unique indices for Genes that need to be skipped
@@ -70,7 +77,7 @@ skip_index = unique(c(index_cancer, index_healthy))
 GE_lusc.cancer.clean = GE_lusc.cancer[-skip_index,]
 GE_lusc.healthy.clean = GE_lusc.healthy[-skip_index,]
 
-#initialize clean gene list of key value pair 
+# Initialize clean gene list of key value pair 
 GE_lusc.cancer.keyVal <- list()
 GE_lusc.healthy.keyVal<- list()
 for(Gene in rownames(GE_lusc.cancer.clean))
@@ -82,17 +89,18 @@ for(Gene in rownames(GE_lusc.cancer.clean))
   ## index of patients with GE level of Cancerous tissues = 0
   patient_index2 = which(GE_lusc.cancer.clean[Gene,]==0)
   
+  #combine patient_index1, patient_index2 to skip them
   skip_patient_index = unique(c(patient_index1, patient_index2))
   if (length(skip_patient_index)!=0)
-   { 
+  { 
     GE_lusc.cancer.clean2 = GE_lusc.cancer.clean2[-skip_patient_index]
     GE_lusc.healthy.clean2 = GE_lusc.healthy.clean2[-skip_patient_index]
-    }
+  }
   else
-    {
-      GE_lusc.cancer.clean2 = GE_lusc.cancer.clean2
-      GE_lusc.healthy.clean2 = GE_lusc.healthy.clean2
-    }
+  {
+    GE_lusc.cancer.clean2 = GE_lusc.cancer.clean2
+    GE_lusc.healthy.clean2 = GE_lusc.healthy.clean2
+  }
   #fill list of key value pair gene :samples
   GE_lusc.cancer.keyVal[[Gene]]<-GE_lusc.cancer.clean2
   GE_lusc.healthy.keyVal[[Gene]] <-GE_lusc.healthy.clean2
