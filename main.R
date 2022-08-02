dc9484cbe372c2c34d0d5013cbbd124e8adf8b4e
muhamedyoussry <mohamed.ahmed973@eng-st.cu.edu.eg>
2022-08-02 10:54:08 +0200

    small edits

--- main.R
+++ main.R
@@ -9,16 +9,17 @@ GE_kirc.healthy <- read.table("./Project_Data/kirc-rsem-fpkm-tcga_paired.txt", h
 kirc_CNV <- read.table("./Project_Data/kirc_CNV_core.txt", header = T, row.names=1)
 lusc_CNV <- read.table("./Project_Data/lusc_CNV_core.txt", header = T, row.names=1)
 
-gene_profile_example = GE_lusc.cancer.keyVal[[15]]
+
+# ======== will need a for loop for the most 5 genes 
+
+gene_profile_example = GE_lusc.cancer.keyVal[["TOMM70A"]]
 
 gene_CNA = extract_gene_CNA_df(gene_profile_example, lusc_CNV)
 
 View(gene_CNA$df)
-print(gene_CNA$y)
+printint(gene_CNA$y)
 
 gene_CNA_regression(gene_CNA$df ,gene_CNA$y )
-
-
 
 
 
